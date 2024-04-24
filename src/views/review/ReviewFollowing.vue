92f7e3d67da32e5fbabfb8cfe8d5492b0e8f2327
bjh <ekmbjh@naver.com>
2024-04-24 17:41:25 +0900

    #19 feature - review 추천 페이지 작업중

---
+++ src/views/review/ReviewFollowing.vue
@@ -0,0 +1,351 @@
+<template>
+  <div class="contents">
+    <div class="tab-menu sticky" style="z-index: 11">
+      <ul>
+        <li class="tab-menu">
+          <a><span>팔로잉</span></a>
+        </li>
+        <li class="active">
+          <a><span>추천</span></a>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <div
+        class="timeline-post-item timeline-recom-follow"
+        id="influencerList_0"
+      >
+        <div class="__follow-list">
+          <div class="swiper">
+            <div class="swiper-wrapper">
+              <div class="v-scroll">
+                <div class="v-scroll-inner">
+                  <div class="" style="display: flex; flex-wrap: nowrap">
+                    <div
+                      class="swiper-slide"
+                      id="influencser_0_0"
+                      style="margin-right: 12px"
+                    >
+                      <div class="__follow-list-item">
+                        <div class="__user-info">
+                          <div class="profile">
+                            <div class="profile-pic">
+                              <div
+                                class="img"
+                                style="
+                                  background-image: url('../../img/review/profile_default_v2.png');
+                                "
+                              ></div>
+                            </div>
+                            <h4 class="name username">
+                              <span class="txt">펭귄은정어리를사랑해</span>
+                            </h4>
+                          </div>
+                        </div>
+                        <button
+                          type="button"
+                          class="btn btn-orange btn-rounded"
+                        >
+                          <span class="label">팔로우</span>
+                        </button>
+                      </div>
+                    </div>
+                    <div
+                      class="swiper-slide"
+                      id="influencser_1_0"
+                      style="margin-right: 12px"
+                    >
+                      <div class="__follow-list-item">
+                        <div class="__user-info">
+                          <div class="profile">
+                            <div class="profile-pic">
+                              <div
+                                class="img"
+                                style="
+                                  background-image: url('../../img/review/profile_default_v2.png');
+                                "
+                              ></div>
+                            </div>
+                            <h4 class="name username">
+                              <span class="txt">미식가빵야빵야</span>
+                            </h4>
+                          </div>
+                        </div>
+                        <button
+                          type="button"
+                          class="btn btn-orange btn-rounded"
+                        >
+                          <span class="label">팔로우</span>
+                        </button>
+                      </div>
+                    </div>
+                    <div
+                      class="swiper-slide"
+                      id="influencser_2_0"
+                      style="margin-right: 12px"
+                    >
+                      <div class="__follow-list-item">
+                        <div class="__user-info">
+                          <div class="profile">
+                            <div class="profile-pic">
+                              <div
+                                class="img"
+                                style="
+                                  background-image: url('../../img/review/profile_default_v2.png');
+                                "
+                              ></div>
+                            </div>
+                            <h4 class="name username">
+                              <span class="txt">ghostcrp</span>
+                            </h4>
+                          </div>
+                        </div>
+                        <button
+                          type="button"
+                          class="btn btn-orange btn-rounded"
+                        >
+                          <span class="label">팔로우</span>
+                        </button>
+                      </div>
+                    </div>
+                    <div class="swiper-slide" id="influencser_3_0">
+                      <div class="__follow-list-item">
+                        <div class="__user-info">
+                          <div class="profile">
+                            <div class="profile-pic">
+                              <div
+                                class="img"
+                                style="
+                                  background-image: url('../../img/review/profile_default_v2.png');
+                                "
+                              ></div>
+                            </div>
+                            <h4 class="name username">
+                              <span class="txt">kimdahn ee</span>
+                            </h4>
+                          </div>
+                        </div>
+                        <button
+                          type="button"
+                          class="btn btn-orange btn-rounded"
+                        >
+                          <span class="label">팔로우</span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <article class="timeline-post-item timeline-post-item-feed">
+      <div class="card">
+        <!-- 게시글 -->
+        <div class="timeline-header">
+          <!-- 게시글 헤더 -->
+          <div class="profile">
+            <div class="profile-pic">
+              <img
+                height="42"
+                width="42"
+                src="../../img/review/profile_default_v2.png"
+                alt=""
+                class="img"
+              />
+            </div>
+            <div class="__info">
+              <span class="name username">지혜로운 미식가_76561</span>
+              <span class="userinfo">리뷰 1개 </span>
+            </div>
+          </div>
+
+          <button type="button" class="btn btn-orange btn-rounded">
+            <span class="label">팔로우</span>
+          </button>
+        </div>
+
+        <div class="timeline-gallery more" style="border-radius: 4px">
+          <div class="gallery" style="border-radius: 4px">
+            <div class="gallery-in">
+              <div class="imgbox">
+                <div class="imgin">
+                  <img
+                    src="https://ugc-images.catchtable.co.kr/rv/s3cSNdEyRL50KeKUYvOAJug/7e90e1e517c7438db576ac208d8d233b?detail750"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="timeline-post-content">
+          <div class="__post-meta">
+            <div class="_6du64d0">
+              <div class="__rating">334</div>
+              <p
+                class="ooezpq2 _1ltqxco1e"
+                style="--ooezpq0: 4px; --ooezpq1: var(--_1ltqxcoa)"
+              ></p>
+            </div>
+            <div class="__date">1일 전</div>
+          </div>
+          <h3 class="__title"></h3>
+          <div class="__content">
+            <div id="__content1_2650757" class="">
+              부산에 매일 왔었는데 맛있는곳을 제대로 모르고 있었어요.!🥹
+              <br />부산 지인이 저에게 어떻게든 여기 먹어보라고 해서
+              <br />먹었는데 왜 먹어야하는 알것같아요!
+            </div>
+          </div>
+          <div class="restaurant-info">
+            <img
+              src="https://image.toast.com/aaaaaqx/catchtable/shopinfo/s3cSNdEyRL50KeKUYvOAJug/3csndeyrl50kekuyvoajug_2371115245089616_thumbMenuImage.jpg?small220"
+              alt=""
+              class="thumb"
+              style="
+                background: url('data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7')
+                  center center no-repeat transparent;
+              "
+            />
+            <div class="__info">
+              <h4 class="__name"><span>톤쇼우 광안점</span></h4>
+              <div class="__meta">일식 • 부산 광안리</div>
+            </div>
+            <img
+              src="../../img/review/arrow_left_black.svg"
+              alt=""
+              style="
+                background: url('data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7')
+                  center center no-repeat transparent;
+              "
+            />
+          </div>
+        </div>
+
+        <div class="timeline-post-footer _10fm75h6 _10fm75hg _10fm75hj">
+          <div class="__post-meta">
+            <span class="__like la3t9m0" style="transform: none">0</span
+            ><span class="__comment">0</span>
+          </div>
+          <div class="_1vfgwok0">
+            <button type="button" class="_1vfgwok1">
+              <span class="_1e99eu30">MORE</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </article>
+
+    <article class="timeline-post-item timeline-post-item-feed">
+      <div class="card">
+        <!-- 게시글 -->
+        <div class="timeline-header">
+          <!-- 게시글 헤더 -->
+          <div class="profile">
+            <div class="profile-pic">
+              <img
+                height="42"
+                width="42"
+                src="../../img/review/profile_default_v2.png"
+                alt=""
+                class="img"
+              />
+            </div>
+            <div class="__info">
+              <span class="name username">지혜로운 미식가_76561</span>
+              <span class="userinfo">리뷰 1개 </span>
+            </div>
+          </div>
+
+          <button type="button" class="btn btn-orange btn-rounded">
+            <span class="label">팔로우</span>
+          </button>
+        </div>
+
+        <div class="timeline-gallery more" style="border-radius: 4px">
+          <div class="gallery" style="border-radius: 4px">
+            <div class="gallery-in">
+              <div class="imgbox">
+                <div class="imgin">
+                  <img
+                    src="https://ugc-images.catchtable.co.kr/rv/s3cSNdEyRL50KeKUYvOAJug/7e90e1e517c7438db576ac208d8d233b?detail750"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="timeline-post-content">
+          <div class="__post-meta">
+            <div class="_6du64d0">
+              <div class="__rating">5.0</div>
+              <p
+                class="ooezpq2 _1ltqxco1e"
+                style="--ooezpq0: 4px; --ooezpq1: var(--_1ltqxcoa)"
+              ></p>
+            </div>
+            <div class="__date">1일 전</div>
+          </div>
+          <h3 class="__title"></h3>
+          <div class="__content">
+            <div id="__content1_2650757" class="">
+              부산에 매일 왔었는데 맛있는곳을 제대로 모르고 있었어요.!🥹
+              <br />부산 지인이 저에게 어떻게든 여기 먹어보라고 해서
+              <br />먹었는데 왜 먹어야하는 알것같아요!
+            </div>
+          </div>
+          <div class="restaurant-info">
+            <img
+              src="https://image.toast.com/aaaaaqx/catchtable/shopinfo/s3cSNdEyRL50KeKUYvOAJug/3csndeyrl50kekuyvoajug_2371115245089616_thumbMenuImage.jpg?small220"
+              alt=""
+              class="thumb"
+              style="
+                background: url('data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7')
+                  center center no-repeat transparent;
+              "
+            />
+            <div class="__info">
+              <h4 class="__name"><span>톤쇼우 광안점</span></h4>
+              <div class="__meta">일식 • 부산 광안리</div>
+            </div>
+            <img
+              src="../../img/review/arrow_left_black.svg"
+              alt=""
+              style="
+                background: url('data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7')
+                  center center no-repeat transparent;
+              "
+            />
+          </div>
+        </div>
+
+        <div class="timeline-post-footer _10fm75h6 _10fm75hg _10fm75hj">
+          <div class="__post-meta">
+            <span class="__like la3t9m0" style="transform: none">0</span
+            ><span class="__comment">0</span>
+          </div>
+          <div class="_1vfgwok0">
+            <button type="button" class="_1vfgwok1">
+              <span class="_1e99eu30">MORE</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import "@/css/review/review.css";
+</style>
