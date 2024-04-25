2b5a04122d9ba799fba179afb425fdca2887c831
bjh <ekmbjh@naver.com>
2024-04-25 17:52:47 +0900

    #19 추천 페이지, 리뷰 작성 페이지 작성

--- src/views/review/ReviewRecommended.vue
+++ src/views/review/ReviewRecommended.vue
@@ -1,104 +1,20 @@
 <template>
   <div class="contents">
+
     <div class="tab-menu sticky" style="z-index: 11">
       <ul>
         <a role="button" class="back-button">
           <img src="@/img/back_arrow.png">
         </a>
-        <li class="active">
+        <li class="tab-menu">
           <a><span>팔로잉</span></a>
         </li>
-        <li class="tab-menu">
+        <li class="active">
           <a><span>추천</span></a>
         </li>
       </ul>
     </div>
 
-    <div>
-      <div class="timeline-post-item timeline-recom-follow" id="influencerList_0">
-        <div class="__follow-list">
-          <div class="swiper">
-            <div class="swiper-wrapper">
-              <div class="v-scroll">
-                <div class="v-scroll-inner">
-                  <div class="" style="display: flex; flex-wrap: nowrap">
-                    <div class="swiper-slide" id="influencser_0_0" style="margin-right: 12px">
-                      <div class="__follow-list-item">
-                        <div class="__user-info">
-                          <div class="profile">
-                            <div class="profile-pic">
-                              <img height="42" width="42" src="../../img/review/profile_default_v2.png" alt="" class="img" />
-                            </div>
-                            <h4 class="name username">
-                              <span class="txt">펭귄은정어리를사랑해</span>
-                            </h4>
-                          </div>
-                        </div>
-                        <button type="button" class="btn btn-orange btn-rounded">
-                          <span class="label">팔로우</span>
-                        </button>
-                      </div>
-                    </div>
-                    <div class="swiper-slide" id="influencser_1_0" style="margin-right: 12px">
-                      <div class="__follow-list-item">
-                        <div class="__user-info">
-                          <div class="profile">
-                            <div class="profile-pic">
-                              <img height="42" width="42" src="../../img/review/profile_default_v2.png" alt="" class="img" />
-                            </div>
-                            <h4 class="name username">
-                              <span class="txt">미식가빵야빵야</span>
-                            </h4>
-                          </div>
-                        </div>
-                        <button type="button" class="btn btn-orange btn-rounded">
-                          <span class="label">팔로우</span>
-                        </button>
-                      </div>
-                    </div>
-                    <div class="swiper-slide" id="influencser_2_0" style="margin-right: 12px">
-                      <div class="__follow-list-item">
-                        <div class="__user-info">
-                          <div class="profile">
-                            <div class="profile-pic">
-                              <img height="42" width="42" src="../../img/review/profile_default_v2.png" alt="" class="img" />
-                            </div>
-                            <h4 class="name username">
-                              <span class="txt">ghostcrp</span>
-                            </h4>
-                          </div>
-                        </div>
-                        <button type="button" class="btn btn-orange btn-rounded">
-                          <span class="label">팔로우</span>
-                        </button>
-                      </div>
-                    </div>
-                    <div class="swiper-slide" id="influencser_3_0">
-                      <div class="__follow-list-item">
-                        <div class="__user-info">
-                          <div class="profile">
-                            <div class="profile-pic">
-                              <img height="42" width="42" src="../../img/review/profile_default_v2.png" alt="" class="img" />
-                            </div>
-                            <h4 class="name username">
-                              <span class="txt">kimdahn ee</span>
-                            </h4>
-                          </div>
-                        </div>
-                        <button type="button" class="btn btn-orange btn-rounded">
-                          <span class="label">팔로우</span>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    
     <article class="timeline-post-item timeline-post-item-feed">
       <div class="card">
         <!-- 게시글 -->
@@ -114,6 +30,9 @@
             </div>
           </div>
 
+          <button type="button" class="btn btn-orange btn-rounded">
+            <span class="label">팔로우</span>
+          </button>
         </div>
 
         <div class="timeline-gallery more" style="border-radius: 4px">
@@ -202,18 +121,17 @@
       <div style="height: 40px;">
       </div>
     </article>
-
   </div>
 </template>
 
 <script>
 
 export default {
-  components: {
-  }
-};
+
+}
 </script>
 
 <style>
 @import "@/css/review/review.css";
+
 </style>
