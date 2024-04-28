597a5cead38252cc5f241ab210a8c43d701351a0
ekmbjh <ekmbjh@naver.com>
2024-04-28 23:43:14 +0900

    #53 feat: Review Check Page

---
+++ src/views/review/ReviewCheck.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="contents">
+        <div id="__next">
+            <div class="KMrlu_">
+                <div class="_2_9rXJ">
+                    <div class="_1yZXaf"><em>4월 26일 금요일</em>에<br><em>24시 굿케어동물의료센터</em>에 다녀오셨네요!</div>
+                    <div class="_1YJe0G">
+                        <div class="_3vAkIz"><input id="datepicker" type="date" required="" class="_1dVAlO"
+                                min="2019-01-01" max="2024-04-28" value="2024-04-26"><label for="datepicker"
+                                class="_1GVeTV _2lr08j"><span class="_218HBp"><span class="place_blind">방문일</span>4월 26일
+                                    금<span class="place_blind">요일</span></span><span class="_1ZmmJZ">수정</span></label>
+                        </div>
+                        <div class="_3vAkIz"><input id="timepicker" type="time" required="" class="_1dVAlO"
+                                value="21:40:42.000+09:00"><label for="timepicker" class="_1GVeTV _2lr08j"><span
+                                    class="_218HBp"><span class="place_blind">방문시간</span><time aria-hidden="true">오후
+                                        9:40</time><span class="place_blind">Invalid Date</span></span><span
+                                    class="_1ZmmJZ">수정</span></label></div>
+                    </div>
+                    <div class="_3CUr7k">방문하신 날짜와 시간을 확인해 주세요.</div>
+                    <div class="TqODhl">
+                        <div class="_20-WtC"><img
+                                src="https://simg.pstatic.net/static.map/v2/map/staticmap.bin?caller=myPlace&amp;scale=2&amp;w=384&amp;h=149&amp;markers=type:d|size:mid|pos:126.9570268 37.4798919|viewSizeRatio:0.5|color:0x029DFF&amp;logo=false&amp;center=126.9570268 37.4798919&amp;level=13"
+                                class="_1fGAc5 TzorUl" width="100%" height="149" alt=""
+                                style="opacity: 1; transition: opacity 0.3s ease-in 0s;"></div>
+                        <div class="_3UD8KO">
+                            <div class="n0mNgg"><span class="_13qLkt">24시 굿케어동물의료센터</span><span
+                                    class="ax7rl9">동물병원</span></div>
+                            <div class="F6M5e4">서울특별시 관악구 남부순환로 1861 1층 103호</div>
+                            <div class="F6M5e4 _3sydYv"><span class="_19Jzok">지번</span>봉천동 1673-21</div>
+                            <div class="Cktyqc">
+                                <div class="iUeBhZ"><img
+                                        src="https://search.pstatic.net/common/?autoRotate=true&amp;type=w560_sharpen&amp;src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20190411_237%2F1554993989106xMSXW_JPEG%2FFCaZ_wk3jEWniI_RKHLae5gJ.JPG.jpg"
+                                        class="_1fGAc5 _33SlL6" width="100%" height="100%" alt="24시 굿케어동물의료센터"
+                                        style="opacity: 1; transition: opacity 0.3s ease-in 0s;"></div>
+                                <div class="iUeBhZ"><img
+                                        src="https://search.pstatic.net/common/?autoRotate=true&amp;type=w560_sharpen&amp;src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20190411_203%2F155499398881979gxI_JPEG%2Fg9j2cbN1NQOl3BdkjQnKT40M.JPG.jpg"
+                                        class="_1fGAc5 _33SlL6" width="100%" height="100%" alt="24시 굿케어동물의료센터"
+                                        style="opacity: 1; transition: opacity 0.3s ease-in 0s;"></div>
+                                <div class="iUeBhZ"><img
+                                        src="https://search.pstatic.net/common/?autoRotate=true&amp;type=w560_sharpen&amp;src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20190411_96%2F1554993992822pHrYS_JPEG%2Fkq_v7rNhCLF6i1xyJxJLsS9o.JPG.jpg"
+                                        class="_1fGAc5 _33SlL6" width="100%" height="100%" alt="24시 굿케어동물의료센터"
+                                        style="opacity: 1; transition: opacity 0.3s ease-in 0s;"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="_1SkyVO"><button class="wTaI4v _2O98Pe" type="button">이 장소가 아니에요</button><button
+                        class="wTaI4v _2O98Pe m1ObFj" type="button"><svg viewBox="0 0 21 15" class="w2l_T3"
+                            aria-hidden="true">
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M19.843 1.11612C20.3312 1.60427 20.3312 2.39573 19.843 2.88388L8.74766 13.9793C8.25951 14.4674 7.46805 14.4674 6.9799 13.9793L1.11612 8.11548C0.627961 7.62733 0.627961 6.83587 1.11612 6.34772C1.60427 5.85956 2.39573 5.85956 2.88388 6.34772L7.86378 11.3276L18.0753 1.11612C18.5634 0.627961 19.3549 0.627961 19.843 1.11612Z">
+                            </path>
+                        </svg>이 장소가 맞아요</button></div><button class="wTaI4v _2Eh7SF" type="button"><svg
+                        viewBox="0 0 20 20" class="w2l_T3" aria-hidden="true">
+                        <path
+                            d="M10.0031 8.9614L1.70644 0.646057L0.646484 1.7084L8.94314 10.0237L0.69384 18.2916L1.75379 19.354L10.0031 11.0861L18.2455 19.347L19.3054 18.2847L11.063 10.0237L19.3528 1.71532L18.2928 0.652981L10.0031 8.9614Z">
+                        </path>
+                    </svg><span class="place_blind">닫기</span></button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import '@/css/review/review.css';
+.contents {
+  width: 400px;
+  margin: auto;
+}
+</style>
