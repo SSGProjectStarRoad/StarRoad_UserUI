10b752d0ac19c32598cccc5cc62de9acfcf32de6
HRiver <sanscout@naver.com>
2024-05-16 18:36:31 +0900

    #95 component: make back arrow

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import BackButton from './components/Backbutton.vue';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+  .component('BackButton', BackButton)
+  .use(store)
+  .use(router)
+  .mount('#app');
