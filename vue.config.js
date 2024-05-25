f54ec6f2f845e23ba4291a5bd4371c77b4ff9b86
HRiver <sanscout@naver.com>
2024-05-25 14:46:10 +0900

    fix: fix main.js

--- vue.config.js
+++ vue.config.js
@@ -9,4 +9,13 @@ module.exports = {
       },
     },
   },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 프로덕션에서도 Vue DevTools 활성화
+      config.devtool = 'source-map';
+      config.optimization = {
+        minimize: false,
+      };
+    }
+  },
 };
