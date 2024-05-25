68d72aabfd23ecd5500b7810b3cc856fd19b8aaf
HRiver <sanscout@naver.com>
2024-05-25 14:03:08 +0900

    fix: fix main.js

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ const app = createApp(App);
 
 // Vue DevTools를 상시 활성화
 app.config.devtools = true;
-Vue.config.devtools = true;
 console.log('Vue DevTools enabled:', app.config.devtools); // 확인용 로그
 
 app.component('BackButton', BackButton).use(store).use(router).mount('#app');
